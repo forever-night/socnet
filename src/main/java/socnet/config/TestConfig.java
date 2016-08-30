c056d0d3cab604c5b00cfbb32db493999bf88562
forever-night <anna.upintheair@gmail.com>
2016-08-30 13:18:31 +0200

    configured H2 as embedded database for testing

--- src/main/java/socnet/config/TestConfig.java
+++ src/main/java/socnet/config/TestConfig.java
@@ -1,26 +1,36 @@
 package socnet.config;
 
-import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-
-import javax.sql.DataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 
 @Configuration
 @ComponentScan("socnet")
-@Import({JpaConfig.class})
+@Import(JpaConfig.class)
 public class TestConfig {
     @Bean
-    DataSource dataSource() {
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setUrl("jdbc:postgresql://localhost:5432/socnet");
-        dataSource.setUsername("user");
-        dataSource.setPassword("user");
-        dataSource.setDriverClassName("org.postgresql.Driver");
+    EmbeddedDatabase dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setName("jdbc:h2:mem:socnet_test;IGNORECASE=TRUE;")
+                .setType(EmbeddedDatabaseType.H2)
+                .addScripts("testdb.sql")
+                .build();
+    }
+    
+    @Bean
+    JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setDatabase(Database.H2);
+        vendorAdapter.setShowSql(false);
         
-        return dataSource;
+        return vendorAdapter;
     }
 }
---
+++ src/test/resources/testdb.sql
@@ -0,0 +1,87 @@
+set mode PostgreSQL;
+
+create table account (
+  id serial primary key,
+  login varchar(24),
+  email varchar(128),
+  password varchar(128),
+  created_at timestamptz,
+  version int
+);
+
+
+create table profile (
+  id int primary key,
+  name varchar(128),
+  country varchar(64),
+  current_city varchar(64),
+  phone varchar(16),
+  birth_date date,
+  info text,
+  version int,
+  foreign key (id) references account(id)
+);
+
+
+create table profile_follower (
+  owner_id int references profile(id),
+  follower_id int references profile(id)
+);
+
+
+create table private_message (
+  id serial primary key,
+  sender_id int references profile(id),
+  receiver_id int references profile(id),
+  text_content text,
+  created_at timestamptz
+);
+
+
+create table public_message (
+  id serial primary key,
+  sender_id int references profile(id),
+  text_content text,
+  receiver_id int references profile(id),
+  created_at timestamptz,
+  version int
+);
+
+
+create table community (
+  id serial primary key,
+  title varchar(128),
+  owner_id int references profile(id),
+  created_at timestamptz,
+  version int
+);
+
+
+create table community_admin (
+  community_id int references community(id),
+  admin_id int references profile(id)
+);
+
+
+create table community_follower (
+  owner_id int references community(id),
+  follower_id int references profile(id)
+);
+
+
+create table community_message (
+  id serial primary key,
+  sender_id int references community(id),
+  text_content text,
+  created_at timestamptz,
+  version int
+);
+
+alter table profile
+  rename column id to account_id;
+
+alter table account
+  add column role varchar(16);
+
+alter table account
+  add column enabled boolean not null default true;
